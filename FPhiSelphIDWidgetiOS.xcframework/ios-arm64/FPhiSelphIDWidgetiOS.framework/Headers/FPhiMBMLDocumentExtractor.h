8ae81f3117038ae8815a35746f25d2a1e4d6bf80
Facephi <developer@facephi.com>
2024-04-23 09:43:27 +0000

    Delete frameworks and resources

--- FPhiSelphIDWidgetiOS.xcframework/ios-arm64/FPhiSelphIDWidgetiOS.framework/Headers/FPhiMBMLDocumentExtractor.h
+++
@@ -1,9 +0,0 @@
-#ifndef FPhiMBMLDocumentExtractor_h
-#define FPhiMBMLDocumentExtractor_h
-
-#import "IFPhiDocumentExtractor.h"
-
-
-@interface FPhiMBMLDocumentExtractor : IFPhiDocumentExtractor
-@end
-#endif
