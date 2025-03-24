0d871a0e709e2673844c0d3ff03da04f758e67a6
Facephi <developer@facephi.com>
2025-03-24 15:28:21 +0000

    Delete frameworks and resources

--- FPhiSelphIDWidgetiOS.xcframework/ios-x86_64-simulator/FPhiSelphIDWidgetiOS.framework/Headers/FPhiSelphIDWidgetDocumentSide.h
+++
@@ -1,9 +0,0 @@
-#ifndef FPhiSelphIDWidgetDocumentSide_h
-#define FPhiSelphIDWidgetDocumentSide_h
-
-typedef NS_ENUM(NSUInteger, FPhiSelphIDWidgetDocumentSide) {
-    DSFront,
-    DSBack,
-};
-
-#endif /* FPhiSelphIDWidgetDocumentSide_h */
